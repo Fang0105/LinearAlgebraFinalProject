d0c773f30818b4213102826290cea448a14d25aa
Fang0105 <nody0105@gmail.com>
2022-12-14 02:12:18 +0800

    2022/12/14-add city-frequency bar chart

--- final.py
+++ final.py
@@ -47,24 +47,23 @@ plt.rcParams['axes.unicode_minus'] = False
 
 data = pd.read_csv("清交學生回家頻率與影響因素之調查 (回覆) - 表單回應 1.csv")
 
-city = []
-for i in data["居住縣市"]:
-    city.append(i)
-
+cityName = ["基隆市","台北市","新北市","桃園市","新竹縣","新竹市","苗栗縣","台中市","彰化縣","南投縣","雲林縣","嘉義縣","嘉義市","台南市","高雄市","屏東縣","宜蘭縣","花蓮縣","台東縣","澎湖縣","金門縣","連江縣"]
 frequency = []
-for i in data["回家頻率(?週一次，填數字即可)\r\n"]:
-    frequency.append(int(i))
-
 time = []
-for i in data["回家路程時長(單位:小時)\r\n(例:1、2.5)"]:
-    time.append(float(i))
-
 cost = []
-for i in data["回家所需交通費(抓單程，大概即可)"]:
-    cost.append(int(i))
+dict = {}
+for i in cityName:
+    dict[i] = [0,0]
+for index,row in data.iterrows():
+    frequency.append(int(row["回家頻率(?週一次，填數字即可)\r\n"]))
+    time.append(float(row["回家路程時長(單位:小時)\r\n(例:1、2.5)"]))
+    cost.append(int(row["回家所需交通費(抓單程，大概即可)"]))
+    dict[row["居住縣市"]][0]+=1
+    dict[row["居住縣市"]][1]+=row["回家頻率(?週一次，填數字即可)\r\n"]
+
 
 #回家時間-頻率
-plt.subplot(2,1,1)
+plt.subplot(3,1,1)
 plt.scatter(time,frequency)
 plt.xlabel("回家時間(hr)")
 plt.ylabel("多久回家一次(week)")
@@ -84,7 +83,7 @@ plt.plot(xdot,ydot,c='r')
 
 
 #回家花費-頻率
-plt.subplot(2,1,2)
+plt.subplot(3,1,2)
 plt.scatter(cost,frequency)
 plt.xlabel("回家花費(TWD)")
 plt.ylabel("多久回家一次(week)")
@@ -102,4 +101,20 @@ for i in range(0,2000):
     ydot.append(x[0][0]+i*x[1][0])
 plt.plot(xdot,ydot,c='r')
 
+#縣市-頻率
+x = []
+h = []
+label = []
+cnt = 1
+for i in dict:
+    if dict[i][0]!=0:
+        x.append(cnt)
+        cnt+=1
+        h.append(dict[i][1]/dict[i][0])
+        label.append(i)
+plt.subplot(3,1,3)
+plt.bar(x,h,tick_label=label)
+plt.xlabel("縣市")
+plt.ylabel("平均多久回家一次(week)")
+plt.tight_layout()
 plt.show()
